0d2d8519f780414d5ba454f9abb1aa0cc0bf4c66
skycpp <qjzl2008@163.com>
2014-10-16 07:53:08 -0700

    add 002

---
+++ leetcode/leetcode_002.hpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int> > levelOrderBottom(TreeNode *root) {        
+        vector<vector<int> > result, ret;
+        if (!root)
+            return result;
+        queue<TreeNode*> parentQueue;
+        parentQueue.push(root);
+        do
+        {
+            queue<TreeNode* > childQueue;
+            vector<int> curList;
+            while(!parentQueue.empty())
+            {
+                TreeNode* node = parentQueue.front();
+                parentQueue.pop();
+                curList.push_back(node->val);
+                if (node->left)
+                    childQueue.push(node->left);
+                if (node->right)
+                    childQueue.push(node->right);
+            }
+            result.push_back(curList);
+            parentQueue = childQueue;
+        }while(!parentQueue.empty());
+        for (int i = result.size() - 1; i >= 0; --i)
+        {
+            ret.push_back(result[i]);
+        }        
+        return ret;
+    }
+};
