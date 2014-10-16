5087df448633e20c349706e4f8d4af0ed2e1a974
root <root@localhost.localdomain>
2014-10-17 07:20:10 +0800

    heap modify

--- test/heap/Heap.hpp
+++ test/heap/Heap.hpp
@@ -4,60 +4,83 @@
 namespace skycpp
 {
     template<class T>
+
+    // 暂只考虑指针类型的处理，所以屏蔽原始结构的构造
     class Heap
     {
+    private:
+        Heap(){}
+        ~Heap(){}
     };
-    template<class T>
-	class Heap <T *>
-	{
-	public:
-		typedef bool(*TypeOnCompare)(T*, T*);
-		Heap(TypeOnCompare onCompare = NULL): _onCompare(onCompare){}
-		Heap(T* beginElement, int count, TypeOnCompare onCompare = NULL)
-		: _onCompare(onCompare)
-		{
-			for (int i = 0; i < count; ++i)
-			{
-				_dataList.push_back(&beginElement[i]);
-			}
-			if (count > 1)
-			{
-				if (_onCompare)
-                    std::make_heap(_dataList.begin(), _dataList.end(), _onCompare);
-				else
-					std::make_heap(_dataList.begin(), _dataList.end());				
-			}
-		}
-		~Heap(){}
-		
-		void Push(T* t)
-		{
-			_dataList.push_back(t);
-			if (_onCompare)
-				std::push_heap(_dataList.begin(), _dataList.end(), _onCompare);
-			else
-				std::push_heap(_dataList.begin(), _dataList.end());
-		}
-		
-		void Pop()
-		{
-			if (!_dataList.size())
-				return;				
-			if (_onCompare)
-				std::pop_heap(_dataList.begin(), _dataList.end(), _onCompare);
-			else
-				std::pop_heap(_dataList.begin(), _dataList.end());
-            _dataList.pop_back();
-		}
 
-		T* Top()
-		{
-			if (!_dataList.size())
-				return NULL;
-			return _dataList[0];
-		}
-	protected:
+    template<class T>
+    class Heap <T *>
+    {
+    public:
+        // 二元判断式，(t0 < t1) == true时为大顶堆，(t0 > t1) == true 时为小顶堆
+        typedef bool(*TypeOnCompare)(T*, T*);
+        Heap(TypeOnCompare onCompare = NULL): _onCompare(onCompare){}
+
+        Heap(T* beginElement, int count, TypeOnCompare onCompare = NULL)
+        : _onCompare(onCompare)
+        {
+            for (int i = 0; i < count; ++i)
+                _dataList.push_back(&beginElement[i]);
+            if (_onCompare)
+                std::make_heap(_dataList.begin(), _dataList.end(), _onCompare);
+            else
+                std::make_heap(_dataList.begin(), _dataList.end());	
+        }
+        ~Heap()
+        {
+            typename std::vector<T*>::iterator it = _dataList.begin();
+            for (; it != _dataList.end(); ++it)
+                delete *it;
+            _dataList.clear();            
+        }
+
+        inline void Push(T* t)
+        {
+            _dataList.push_back(t);
+            if (_onCompare)
+                std::push_heap(_dataList.begin(), _dataList.end(), _onCompare);
+            else
+                std::push_heap(_dataList.begin(), _dataList.end());
+        }
+
+        inline void Pop()
+        {
+            if (this->isEmpty())
+                return;				
+            if (_onCompare)
+                std::pop_heap(_dataList.begin(), _dataList.end(), _onCompare);
+            else
+                std::pop_heap(_dataList.begin(), _dataList.end());
+            _dataList.pop_back();
+        }
+
+        inline T* Top()
+        {
+            if (this->isEmpty())
+                return NULL;
+            return _dataList[0];
+        }
+
+        inline void AdjustHeap(TypeOnCompare onCompare = NULL)
+        {
+            _onCompare = onCompare;
+            if (_onCompare)
+                std::make_heap(_dataList.begin(), _dataList.end(), _onCompare);
+            else
+                std::make_heap(_dataList.begin(), _dataList.end());
+        }
+        // 堆是否为空
+        inline bool isEmpty() const {return _dataList.empty();}
+        // 堆大小
+        inline size_t size() const {return _dataList.size();}
+    protected:
         std::vector<T*> _dataList;
-		TypeOnCompare _onCompare;
-	};
+        TypeOnCompare _onCompare;
+    };
 }
+
