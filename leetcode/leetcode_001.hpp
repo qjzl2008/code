faa850ee75c8fc40412b0ec88252cc3596a6bafd
root <root@localhost.localdomain>
2014-10-16 07:25:51 -0700

    create leetcode

---
+++ leetcode/leetcode_001.hpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int result = 0;
+        while(x != 0)
+        {
+            int lastX = x;
+            x = x / 10;
+            result = 10 * result + lastX - x * 10;
+        }
+        return result;
+    }
+};
